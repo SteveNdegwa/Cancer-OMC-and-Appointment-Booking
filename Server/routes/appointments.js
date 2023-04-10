170d1d5de74bdfcb6f49eda2db27ee64be366865
Steven Ndegwa <stevencallistus19@gmail.com>
2023-04-10 22:25:55 +0300

    connected book-appointment to the database

--- Server/routes/appointments.js
+++ Server/routes/appointments.js
@@ -6,7 +6,7 @@ const pool = require("../server.js");
 const path = require("path");
 const { log } = require("console");
 
-let doctorId = 1;
+let doctorId = 2;
 
 router.get("/customize-appointment-slots", (req, res) => {
   res.render("appointment-slots");
@@ -47,7 +47,7 @@ router.post("/customize-appointment-slots", (req, res) => {
   let timeJson = JSON.stringify(time);
 
   pool.getConnection((err, connection) => {
-    if (err)console.log(err);
+    if (err) console.log(err);
     else {
       const save = new Promise((resolve, reject) => {
         days.forEach((day) => {
@@ -56,31 +56,134 @@ router.post("/customize-appointment-slots", (req, res) => {
           connection.query(query, [doctorId, day], (err, result) => {
             if (err) console.log(err);
 
-            if(result.length){
-                const query = "UPDATE appointment_slots SET slots=? WHERE doctor_id =? AND day =?";
-                connection.query(query, [timeJson, doctorId, day],(err,data)=>{
-                    if(err)console.log(err);
-                    else{
-                        console.log(`${day} updated successfully`);
-                    }
-                })
-            }
-            
-            else{
-                const query = "INSERT INTO appointment_slots(`doctor_id`, `day`, `slots`) VALUES(?)";
-                const values = [doctorId, day ,timeJson];
-                connection.query(query, [values],(err,data)=>{
-                    if(err)console.log(err);
-                    else{
-                        console.log(`${day} inserted successfully`);
-                    }
-                })
+            if (result.length) {
+              const query =
+                "UPDATE appointment_slots SET slots=? WHERE doctor_id =? AND day =?";
+              connection.query(
+                query,
+                [timeJson, doctorId, day],
+                (err, data) => {
+                  if (err) console.log(err);
+                  else {
+                    console.log(`${day} updated successfully`);
+                  }
+                }
+              );
+            } else {
+              const query =
+                "INSERT INTO appointment_slots(`doctor_id`, `day`, `slots`) VALUES(?)";
+              const values = [doctorId, day, timeJson];
+              connection.query(query, [values], (err, data) => {
+                if (err) console.log(err);
+                else {
+                  console.log(`${day} inserted successfully`);
+                }
+              });
             }
           });
         });
       });
     }
   });
+});
+
+
+
+let appointmentSlots = [];
+let chosenDate = "";
+
+router.get("/book-appointment", (req, res) => {
+  pool.getConnection((err, connection) => {
+    const query =
+      "SELECT name, cancer_speciality, clinic_location, clinic_phone_no, clinic_email FROM doctor_details where user_id = ?";
+    connection.query(query, [doctorId], (err, results) => {
+      if (err) console.log(err);
+      else {
+        console.log(results);
+
+        var string = JSON.stringify(results);
+        var details = JSON.parse(string);
+
+        appointmentSlots = [];
+        chosenDate = ""
+
+        return res.render("book-appointment", {
+          details: details,
+          appointmentSlots: appointmentSlots,
+          chosenDate: chosenDate
+        });
+      }
+    });
+  });
+});
+
+router.post("/book-appointment", (req, res) => {
+
+  let details = [];
+
+  if(req.body.submitType == "date change"){  /// reload after date change
+    
+  const getDetails = new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      const query =
+        "SELECT name, cancer_speciality, clinic_location, clinic_phone_no, clinic_email FROM doctor_details where user_id = ?";
+      connection.query(query, [doctorId], (err, results) => {
+        if (err) throw err;
+        else {
+          console.log(results);
+
+          var string = JSON.stringify(results);
+          details = JSON.parse(string);
+
+          resolve(details);
+
+        }
+      });
+    });
+  });
+
+  getDetails.then((details) => {
+    appointmentSlots = [];
+    chosenDate = req.body.date;
+
+    let date = new Date(req.body.date);
+
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayIndex = date.getDay();
+    let day =  weekdays[dayIndex];
+
+    pool.getConnection((err, connection) => {
+      const query =
+        "SELECT slots FROM appointment_slots where doctor_id = ? AND day = ?";
+      connection.query(query, [1, day], (err, results) => {
+        if (err) throw err;
+
+      if(results.length){
+        console.log(results);
+        console.log(results[0].slots);
+        console.log(JSON.parse(results[0].slots));
+        
+        appointmentSlots = JSON.parse(results[0].slots);
+      }
+
+      return res.render("book-appointment", { details: details, appointmentSlots:appointmentSlots, chosenDate: chosenDate });
+        
+      });
+    });
+
+  });
+  }
+
+
+
+  else{ //// for saving appointments to the database  // check if more than two selected  //add 
+
+    console.log(`Appointments(saving) :  ${appointmentSlots}`);
+    appointmentSlots.forEach((slot)=>{
+      console.log(slot);
+    })
+  }
+
 });
 
 module.exports = router;
