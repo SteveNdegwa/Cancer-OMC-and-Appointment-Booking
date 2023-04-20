7e515ef9b530858ca46643f7f33b33c337eca71e
Steven Ndegwa <stevencallistus19@gmail.com>
2023-04-21 02:23:29 +0300

    verify and unverify doctors logic

--- Server/routes/admin.js
+++ Server/routes/admin.js
@@ -11,7 +11,7 @@ router.get("/view-unverified-doctors",(req,res)=>{
         if(err) throw err;
 
         const query = "SELECT * FROM doctor_details WHERE verification_status = ?";
-        connection.query(query,["unverified"],(err,results)=>{
+        connection.query(query,["false"],(err,results)=>{
             if(err) throw err;
 
             res.render("view-unverified-doctors",{doctors:results});
@@ -25,15 +25,47 @@ router.post("/view-unverified-doctors",(req,res)=>{
     pool.getConnection((err,connection)=>{
         if(err) throw err;
 
-        const status = null;
-
-        const query = "UPDATE doctor_details SET verification_status =? WHERE user_id =?";
-        connection.query(query,[status,req.body.doctor_id],(err,data)=>{
+        const query = "UPDATE doctor_details SET verification_status = ? WHERE user_id =?";
+        connection.query(query,["true",req.body.doctor_id],(err,data)=>{
             if(err) throw err;
             else{
                 console.log(data);
                 console.log("Verified Successfully");
                 res.redirect("/admin/view-unverified-doctors");
+            }
+        })
+
+        connection.release();
+    })
+})
+
+
+router.get("/view-verified-doctors",(req,res)=>{
+    pool.getConnection((err,connection)=>{
+        if(err) throw err;
+
+        const query = "SELECT * FROM doctor_details WHERE verification_status = ?";
+        connection.query(query,["true"],(err,results)=>{
+            if(err) throw err;
+
+            res.render("view-verified-doctors",{doctors:results});
+        })
+
+        connection.release();
+    })
+})
+
+router.post("/view-verified-doctors",(req,res)=>{
+    pool.getConnection((err,connection)=>{
+        if(err) throw err;
+
+        const query = "UPDATE doctor_details SET verification_status = ? WHERE user_id =?";
+        connection.query(query,["false",req.body.doctor_id],(err,data)=>{
+            if(err) throw err;
+            else{
+                console.log(data);
+                console.log("Unverified Successfully");
+                res.redirect("/admin/view-verified-doctors");
             }
         })
 
