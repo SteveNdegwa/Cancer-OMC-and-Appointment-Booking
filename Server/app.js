939fbd03d7f942d605e2ad4a2bd95c4b4f9eeb3c
Steven Ndegwa <stevencallistus19@gmail.com>
2023-05-15 13:52:46 +0300

    adding logic to pay consultation fee

--- Server/app.js
+++ Server/app.js
@@ -61,13 +61,293 @@ app.use("/admin", admin);
 const server = http.createServer(app);
 
 const socketio = require("socket.io");
-const { log } = require("console");
 const io = socketio(server);
 
 let room = "";
 let user = "";
 let userId = "";
 let accountType = "";
+
+
+
+let token = "";
+
+function access(req, res, next) {
+  let request = unirest(
+    "GET",
+    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+  )
+    .headers({
+      Authorization: "Basic " + process.env.AUTHORIZATION_CODE,
+    })
+    .send()
+    .end((response) => {
+      if (response.error) throw new Error(response.error);
+      console.log(response.raw_body);
+      token = JSON.parse(response.raw_body).access_token;
+      next();
+    });
+}
+
+app.get("/chats/pay-consultation-fee", (req, res) => {
+  if(req.session.authenticated){
+  req.flash("consultMsg", "");
+  res.render("pay-consultation-fee.ejs", {
+    message: consultMsg,
+    consultationFee: req.session.consultationFee,
+  });
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.post("/chats/pay-consultation-fee", (req, res) => {
+  if (req.body.home == "") {
+    // to homepage
+    return res.redirect("/");
+  } else if (req.body.view == "") {
+    /// view mode
+    req.session.viewMode = true;
+    return res.redirect("/chats/chat");
+  } else if (req.body.pay == "") {
+    /// pay
+    req.flash("consultationPaymentMessage", "");
+    res.render("consultation-input-number", {
+      message: req.flash("consultationPaymentMessage"),
+    });
+  }
+});
+
+let expiryTime = "";
+
+app.post("/chats/consultation-input-number", access, (req, res) => {
+  pool.getConnection((err, connection) => {
+    let number = req.body.number.substring(1);
+    let mobileNo = "254" + number;
+
+    let amount = req.session.consultationFee;
+
+    const date = new Date();
+    const timestamp =
+      date.getFullYear() +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      ("0" + date.getDate()).slice(-2) +
+      ("0" + date.getHours()).slice(-2) +
+      ("0" + date.getMinutes()).slice(-2) +
+      ("0" + date.getSeconds()).slice(-2);
+
+    const shortcode = process.env.SHORT_CODE2; ////// or req.session.consultation.businessNo
+    const passkey = process.env.PASS_KEY;
+
+    const password = new Buffer.from(shortcode + passkey + timestamp).toString(
+      "base64"
+    );
+
+    let request = unirest(
+      "POST",
+      "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    )
+      .headers({
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      })
+      .send(
+        JSON.stringify({
+          BusinessShortCode: shortcode,
+          Password: password,
+          Timestamp: timestamp,
+          TransactionType: "CustomerPayBillOnline",
+          Amount: amount,
+          PartyA: mobileNo,
+          PartyB: shortcode,
+          PhoneNumber: mobileNo,
+          CallBackURL: "https://02b6-197-237-171-106.ngrok-free.app/callback",
+          AccountReference: "CSS",
+          TransactionDesc: "Payment of Consultation",
+        })
+      )
+      .end((response) => {
+        if (response.error) {
+          console.log(response.error);
+
+          req.flash(
+            "consultMsg",
+            "Sorry. There is a problem generating the STK Push"
+          );
+          return res.render("pay-consultation-fee.ejs", {
+            message: consultMsg,
+            consultationFee: req.session.consultationFee,
+          });
+        }
+        console.log(response.body);
+        console.log(response.body.CheckoutRequestID);
+
+        req.session.consultation.CheckoutRequestID =
+          response.body.CheckoutRequestID;
+
+        if (err) throw err;
+
+        const now = new Date();
+        expiryTime = now.getTime(); //// + 24Hrs;
+
+        const query2 =
+          "INSERT INTO consultations_stk_push(`checkout_id`, `phone_no` ,`amount` , `timestamp` , `doctor_id`, `patient_id`, `consultation_expiry_time`) VALUES(?);";
+        const values2 = [
+          response.body.CheckoutRequestID,
+          mobileNo,
+          amount,
+          timestamp,
+          req.session.consultation.doctorId,
+          req.session.userId,
+          expiryTime,
+        ];
+        connection.query(query2, [values2], (err, data) => {
+          if (err) throw err;
+          console.log("consultation STK data inserted successfully");
+
+          req.flash("consultationStkStatusMessage", "");
+          res.render("consultation-stk-push", {
+            message: req.flash("consultationStkStatusMessage"),
+          });
+        });
+      });
+
+    connection.release();
+  });
+});
+
+app.post("/chats/consultation-stk-push", (req, res) => {
+  const date = new Date();
+  const timestamp =
+    date.getFullYear() +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    ("0" + date.getDate()).slice(-2) +
+    ("0" + date.getHours()).slice(-2) +
+    ("0" + date.getMinutes()).slice(-2) +
+    ("0" + date.getSeconds()).slice(-2);
+
+  const shortcode = process.env.SHORT_CODE2; ////// or req.session.consultation.businessNo
+  const passkey = process.env.PASS_KEY;
+
+  const password = new Buffer.from(shortcode + passkey + timestamp).toString(
+    "base64"
+  );
+
+  let request = unirest(
+    "POST",
+    "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query"
+  )
+    .headers({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    })
+    .send(
+      JSON.stringify({
+        BusinessShortCode: shortcode,
+        Password: password,
+        Timestamp: timestamp,
+        CheckoutRequestID: req.session.consultation.CheckoutRequestID,
+      })
+    )
+    .end((response) => {
+      if (response.error) {
+        req.flash(
+          "consultationStkStatusMessage",
+          "Please complete the payment before proceeding"
+        );
+        return res.render("consultation-stk-push", {
+          message: req.flash("consultationStkStatusMessage"),
+        });
+      }
+      console.log(response.body.ResultDesc);
+
+      if (
+        response.body.ResultDesc ==
+        "The service request is processed successfully."
+      ) {
+        /// if payment successful
+
+        const saveSessions = new Promise((resolve, reject) => {
+          pool.getConnection((err, connection) => {
+            if(err) throw err;
+  
+            const query = "SELECT * FROM consultation_sessions where room_id = ?";
+            connection.query(query,[req.session.roomId],(err,results)=>{
+              if(err) {throw err;}
+              else{
+                if(results.length){
+                  const query2 = "UPDATE consultation_sessions SET expiry_time = ? where room_id = ?";
+                  connection.query(query2,[expiryTime, req.session.roomId],(err,data)=>{
+                    if(err){throw err;}
+                    else{
+                      console.log("consultation session updated successfully");
+                    }
+                  })
+                }else{
+                  const query2 = "INSERT INTO consultation_sessions(`room_id`, `expiry_time`) VALUES(?)";
+                  const values = [
+                    req.session.roomId,
+                    expiryTime
+                  ]
+                  connection.query(query2,[values],(err,data)=>{
+                    if(err){throw err;}
+                    else{
+                      console.log("consultation session inserted successfully");
+                    }
+                  })
+                }
+              }
+            })
+            connection.release();
+          });
+        })
+
+        saveSessions.then(()=>{
+          pool.getConnection((err, connection) => {
+            if (err) throw err;
+            const query = "UPDATE consultations_stk_push SET status = ? WHERE checkout_id = ?";
+            connection.query(
+              query,
+              [
+                response.body.ResultDesc,
+                req.session.paymentDetails.CheckoutRequestID,
+              ],
+              (err, result) => {
+                if (err) {throw err}
+                else{
+                  return res.redirect("/chats/chat");
+                }
+              }
+            );
+          });
+        })
+      } else {
+        /// if payment not successful
+
+        pool.getConnection((err, connection) => {
+          if (err) throw err;
+          const query = "UPDATE consultations_stk_push SET status = ? WHERE checkout_id = ?";
+          connection.query(
+            query,
+            [
+              response.body.ResultDesc,
+              req.session.paymentDetails.CheckoutRequestID,
+            ],
+            (err, result) => {
+              if (err) throw err;
+
+              req.flash("consultationPaymentMessage", response.body.ResultDesc);
+              return res.render("consultation-input-number", {
+                message: req.flash("consultationPaymentMessage"),
+              });
+            }
+          );
+        });
+      }
+    });
+});
+
+
 
 app.get("/chats/chat-rooms", (req, res) => {
   let rooms = [];
@@ -122,24 +402,28 @@ app.get("/chats/chat-rooms", (req, res) => {
               rooms[i].name = results[0].name;
 
               const query3 = "SELECT * FROM chats WHERE room_id = ?";
-              connection.query(query3,[rooms[i].room_id],(err,messages)=>{     ////// get last message in the chat room   /// and sender name
-                if(err) throw err;
-                if(messages.length){
-                  rooms[i].message = messages[messages.length-1].message;
-                  rooms[i].date = messages[messages.length-1].date;
-                  rooms[i].time = messages[messages.length-1].time;
-                  if((messages[messages.length-1].sender_id) == req.session.userId){
+              connection.query(query3, [rooms[i].room_id], (err, messages) => {
+                ////// get last message in the chat room   /// and sender name
+                if (err) throw err;
+                if (messages.length) {
+                  rooms[i].message = messages[messages.length - 1].message;
+                  rooms[i].date = messages[messages.length - 1].date;
+                  rooms[i].time = messages[messages.length - 1].time;
+                  if (
+                    messages[messages.length - 1].sender_id ==
+                    req.session.userId
+                  ) {
                     rooms[i].sender = "You";
-                  }else{
+                  } else {
                     rooms[i].sender = results[0].name;
                   }
-                }else{
+                } else {
                   rooms[i].message = "";
                 }
                 if (i == rooms.length - 1) {
-                resolve(rooms);
-              }
-              })
+                  resolve(rooms);
+                }
+              });
             });
           }
         } else {
@@ -212,7 +496,6 @@ io.on("connection", (socket) => {
       if (err) throw err;
       if (results.length) {
         results.forEach((message) => {
-
           let query2 = "";
           if (accountType == "patient") {
             query2 = "SELECT name FROM doctor_details WHERE user_id = ?";
