df373fd93a223392d588794571f92346a25d2ec1
Steven Ndegwa <stevencallistus19@gmail.com>
2023-05-15 12:25:49 +0300

    adding logic to check if the consultation is paid or free after clicking consult button

--- Server/routes/home.js
+++ Server/routes/home.js
@@ -41,9 +41,9 @@ router.get("/", (req, res) => {
                   return res.redirect("/register/patient/medical-details");
                 } else {
                   getDoctorsData.then((results) => {
-                    if(req.session.accountType == "patient"){
+                    if (req.session.accountType == "patient") {
                       return res.render("index", { doctors: results });
-                    }else{
+                    } else {
                       return res.render("index2");
                     }
                   });
@@ -70,9 +70,9 @@ router.get("/", (req, res) => {
                   return res.redirect("/register/doctor/professional-details");
                 } else {
                   getDoctorsData.then((results) => {
-                    if(req.session.accountType == "patient"){
+                    if (req.session.accountType == "patient") {
                       return res.render("index", { doctors: results });
-                    }else{
+                    } else {
                       return res.render("index2");
                     }
                   });
@@ -93,10 +93,136 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    
   if (req.body.consult == "") {
     /// consultations
     console.log("consult");
+
+    const getRoomId = new Promise((resolve, reject) => {
+      pool.getConnection((err, connection) => {
+        if (err) throw err;
+        const query =
+          "SELECT * FROM chat_rooms WHERE patient_id =? AND doctor_id =?";
+        connection.query(
+          query,
+          [req.session.userId, req.body.doctor_id],
+          (err, results) => {
+            if (err) {
+              throw err;
+            } else {
+              let roomId = "";
+
+              if (results.length) {
+                roomId = results[0].room_id;
+                req.session.roomId = roomId;
+                resolve(roomId);
+              } else {
+                const query2 =
+                  "INSERT INTO chat_rooms(`room_id`, `patient_id`, `doctor_id`) VALUES(?)";
+                const values = [
+                  `room-${req.session.userId}-${req.body.doctor_id}`,
+                  req.session.userId,
+                  req.body.doctor_id,
+                ];
+                connection.query(query2, [values], (err, data) => {
+                  if (err) throw err;
+                  console.log("room created successfully ");
+
+                  roomId = `room-${req.session.userId}-${req.body.doctor_id}`;
+                  req.session.roomId = roomId;
+                  resolve(roomId);
+                });
+              }
+            }
+          }
+        );
+      });
+    });
+
+    getRoomId.then((roomId) => {
+      const checkConsultationType = new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+          if (err) {
+            throw err;
+          } else {
+            let freeConsultation;
+            const query =
+              "SELECT consultation_type FROM doctor_payment_details WHERE doctor_id = ?";
+            connection.query(query, [req.body.doctor_id], (err, result) => {
+              if (err) {
+                throw err;
+              } else {
+                if (result[0].consultation_type == "free") {
+                  freeConsultation = true;
+                } else {
+                  /// paid
+                  freeConsultation = false;
+                }
+              }
+            });
+          }
+
+          connection.release();
+        });
+      });
+
+      checkConsultationType.then((freeConsultation) => {
+        if (freeConsultation) {
+          ///// to free consultation
+          req.session.consultationType = "free";
+          return res.redirect("/chats/chat");
+        } else {
+          //// to paid consultation
+
+          req.session.consultationType = "paid";
+
+          const checkSessionStatus = new Promise((resolve, reject) => {
+            pool.getConnection((err, connection) => {
+              if (err) {
+                throw err;
+              } else {
+                const query =
+                  "SELECT expiry_time FROM consultation_sessions where room_id = ?";
+                connection.query((err, result) => {
+                  if (err) {
+                    throw err;
+                  } else {
+                    if (result.length) {
+                      let expiryTime = result[0].expiry_time;
+
+                      const now = new Date();
+                      if (now.getTime() > expiryTime) {
+                        let sessionActive = false;
+                        resolve(sessionActive);
+                      } else {
+                        let sessionActive = true;
+                        req.session.expiryTime = expiryTime;
+                        resolve(sessionActive);
+                      }
+                    } else {
+                      /// no payment made --- redirect to payment page
+                      let sessionActive = false;
+                      resolve(sessionActive);
+                    }
+                  }
+                });
+              }
+
+              connection.release();
+            });
+          });
+
+          checkSessionStatus.then((sessionActive) => {
+            if (sessionActive) {
+              //// session still active
+              return res.redirect("/chats/chat");
+            } else {
+              /// session expired
+              return res.redirect("/chats/pay-consultation-fee");
+            }
+          });
+        }
+      });
+    });
 
     pool.getConnection((err, connection) => {
       if (err) throw err;
@@ -129,10 +255,7 @@ router.post("/", (req, res) => {
         }
       );
     });
-
-  } 
-  
-  else if (req.body.book == "") {
+  } else if (req.body.book == "") {
     /// bookings
     req.session.doctorId = req.body.doctor_id;
     req.session.bookingType = "new";
