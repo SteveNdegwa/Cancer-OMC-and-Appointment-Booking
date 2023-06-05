c3adbb7f0971a7bd4ceee0489611620eb8c94d50
Steven Ndegwa <stevencallistus19@gmail.com>
2023-06-05 17:02:13 +0300

    adding logic to retrieve and update doctor payment details in doctor profile

--- Server/routes/home.js
+++ Server/routes/home.js
@@ -593,18 +593,54 @@ router.get("/my-profile", (req, res) => {
         });
       });
       getDetails.then((details) => {
-        let male = "",
-          female = "";
-        if (details.gender == "male") {
-          male = "checked";
-        } else {
-          female = "checked";
-        }
-        res.render("my-profile-doctor", {
-          details: details,
-          male: male,
-          female: female,
-          message: req.flash("myProfileMsg"),
+        const getPaymentDetails = new Promise((resolve, reject) => {
+          pool.getConnection((err, connection) => {
+            if (err) {
+              throw err;
+            } else {
+              const query =
+                "SELECT * FROM doctor_payment_details WHERE doctor_id = ?";
+              connection.query(query, [req.session.userId], (err, results) => {
+                if (err) {
+                  throw err;
+                } else {
+                  details.businessNo = results[0].business_no;
+                  details.consultationType = results[0].consultation_type;
+                  details.appointmentFee = results[0].appointment_fee;
+                  details.consultationFee = results[0].consultation_fee;
+
+                  resolve(details);
+                }
+              });
+            }
+
+            connection.release();
+          });
+        });
+        getPaymentDetails.then((details) => {
+          let male = "",
+            female = "";
+          if (details.gender == "male") {
+            male = "checked";
+          } else {
+            female = "checked";
+          }
+
+          let paid = "";
+          let free = "";
+          if (details.consultationType == "free") {
+            free = "checked";
+          } else {
+            paid = "checked";
+          }
+          res.render("my-profile-doctor", {
+            details: details,
+            male: male,
+            female: female,
+            free: free,
+            paid: paid,
+            message: req.flash("myProfileMsg"),
+          });
         });
       });
     }
@@ -730,99 +766,182 @@ router.post("/my-profile", (req, res) => {
       });
     });
     getDetails.then((details) => {
-      let male = "",
-        female = "";
-      if (details.gender == "male") {
-        male = "checked";
-      } else {
-        female = "checked";
-      }
-
-      var reqExp = /[a-zA-Z]/; /// check also symbols
-
-      if (
-        reqExp.test(req.body.phone) ||
-        reqExp.test(req.body.clinic_phone_no)
-      ) {
-        //letters in phone
-        req.flash("myProfileMsg", "Invalid Phone No.");
-        return res.render("my-profile-doctor", {
-          details: details,
-          male: male,
-          female: female,
-          message: req.flash("myProfileMsg"),
-        });
-      } else {
-        const verifyEmail = new Promise((resolve, reject) => {
-          /// verify email
-          resolve(emailValidator.validate(req.body.clinic_email));
-        });
-        verifyEmail.then((data) => {
-          if (data.valid) {
-            pool.getConnection((err, connection) => {
-              if (err) {
-                req.flash("myProfileMsg", "Error Connecting To The Database");
-                return res.render("my-profile-doctor", {
-                  details: details,
-                  male: male,
-                  female: female,
-                  message: req.flash("myProfileMsg"),
-                });
-              } else {
-                let gender = "";
-                if (req.body.male == "on") {
-                  gender = "male";
-                } else {
-                  gender = "female";
-                }
-                const query =
-                  "UPDATE doctor_details SET name = ?, gender = ?, dob = ?, phone_no =?, licence_no= ?, cancer_speciality = ?, clinic_location= ?, clinic_phone_no = ?, clinic_email= ? WHERE user_id = ?";
-
-                connection.query(
-                  query,
-                  [
-                    req.body.name,
-                    gender,
-                    req.body.dob,
-                    req.body.phone,
-                    req.body.licence_no,
-                    req.body.cancer_speciality,
-                    req.body.clinic_location,
-                    req.body.clinic_phone_no,
-                    req.body.clinic_email,
-                    req.session.userId,
-                  ],
-                  (err, data) => {
-                    if (err) {
-                      req.flash("myProfileMsg", "Error Saving The Details");
-                      return res.render("my-profile-doctor", {
-                        details: details,
-                        male: male,
-                        female: female,
-                        message: req.flash("myProfileMsg"),
-                      });
-                    } else {
-                      console.log("Doctor details updated");
-
-                      return res.redirect("/my-profile");
-                    }
-                  }
-                );
-              }
-              connection.release();
-            });
+      const getPaymentDetails = new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+          if (err) {
+            throw err;
           } else {
-            /// invalid email
-            req.flash("myProfileMsg", "Invalid Email Address");
-            return res.render("my-profile-doctor", {
-              details: details,
-              male: male,
-              female: female,
-              message: req.flash("myProfileMsg"),
+            const query =
+              "SELECT * FROM doctor_payment_details WHERE doctor_id = ?";
+            connection.query(query, [req.session.userId], (err, results) => {
+              if (err) {
+                throw err;
+              } else {
+                details.businessNo = results[0].business_no;
+                details.consultationType = results[0].consultation_type;
+                details.appointmentFee = results[0].appointment_fee;
+                details.consultationFee = results[0].consultation_fee;
+
+                resolve(details);
+              }
             });
           }
+
+          connection.release();
         });
-      }
+      });
+      getPaymentDetails.then((details) => {
+        let male = "",
+          female = "";
+        if (details.gender == "male") {
+          male = "checked";
+        } else {
+          female = "checked";
+        }
+
+        let paid = "";
+        let free = "";
+        if (details.consultationType == "free") {
+          free = "checked";
+        } else {
+          paid = "checked";
+        }
+
+        var reqExp = /[a-zA-Z]/; /// check also symbols
+
+        if (
+          reqExp.test(req.body.phone) ||
+          reqExp.test(req.body.clinic_phone_no) ||
+          reqExp.test(req.body.business_no)
+        ) {
+          //letters in phone
+          req.flash("myProfileMsg", "Invalid Phone No.");
+          return res.render("my-profile-doctor", {
+            details: details,
+            male: male,
+            female: female,
+            free: free,
+            paid: paid,
+            message: req.flash("myProfileMsg"),
+          });
+        } else {
+          const verifyEmail = new Promise((resolve, reject) => {
+            /// verify email
+            resolve(emailValidator.validate(req.body.clinic_email));
+          });
+          verifyEmail.then((data) => {
+            if (data.valid) {
+              pool.getConnection((err, connection) => {
+                if (err) {
+                  req.flash("myProfileMsg", "Error Connecting To The Database");
+                  return res.render("my-profile-doctor", {
+                    details: details,
+                    male: male,
+                    female: female,
+                    free: free,
+                    paid: paid,
+                    message: req.flash("myProfileMsg"),
+                  });
+                } else {
+                  let gender = "";
+                  if (req.body.male == "on") {
+                    gender = "male";
+                  } else {
+                    gender = "female";
+                  }
+                  const query =
+                    "UPDATE doctor_details SET name = ?, gender = ?, dob = ?, phone_no =?, licence_no= ?, cancer_speciality = ?, clinic_location= ?, clinic_phone_no = ?, clinic_email= ? WHERE user_id = ?";
+
+                  connection.query(
+                    query,
+                    [
+                      req.body.name,
+                      gender,
+                      req.body.dob,
+                      req.body.phone,
+                      req.body.licence_no,
+                      req.body.cancer_speciality,
+                      req.body.clinic_location,
+                      req.body.clinic_phone_no,
+                      req.body.clinic_email,
+                      req.session.userId,
+                    ],
+                    (err, data) => {
+                      if (err) {
+                        req.flash("myProfileMsg", "Error Saving The Details");
+                        return res.render("my-profile-doctor", {
+                          details: details,
+                          male: male,
+                          female: female,
+                          free: free,
+                          paid: paid,
+                          message: req.flash("myProfileMsg"),
+                        });
+                      } else {
+
+                        let consultationFee = "";
+                        let consultationType = "";
+                        if(req.body.free == "on"){
+                          consultationFee = 0;
+                          consultationType = "free";
+                        }else{
+                          consultationFee = req.body.consultation_fee;
+                          consultationType = "paid";
+                        }
+                        const query2 =
+                          "UPDATE doctor_payment_details SET business_no = ?, consultation_type = ?, appointment_fee = ?, consultation_fee =? WHERE doctor_id = ?";
+
+                        connection.query(
+                          query2,
+                          [
+                            req.body.business_no,
+                            consultationType,
+                            req.body.appointment_fee,
+                            consultationFee,
+                            req.session.userId,
+                          ],
+                          (err, data) => {
+                            if (err) {
+                              req.flash(
+                                "myProfileMsg",
+                                "Error Saving The Details"
+                              );
+                              return res.render("my-profile-doctor", {
+                                details: details,
+                                male: male,
+                                female: female,
+                                free: free,
+                                paid: paid,
+                                message: req.flash("myProfileMsg"),
+                              });
+                            } else {
+                              console.log("Doctor details updated");
+
+                              return res.redirect("/my-profile");
+                            }
+                          }
+                        );
+                      }
+                    }
+                  );
+                }
+                connection.release();
+              });
+            } else {
+              /// invalid email
+              req.flash("myProfileMsg", "Invalid Email Address");
+              return res.render("my-profile-doctor", {
+                details: details,
+                male: male,
+                female: female,
+                free: free,
+                paid: paid,
+                message: req.flash("myProfileMsg"),
+              });
+            }
+          });
+        }
+      });
     });
   }
 });
